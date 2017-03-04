be2663999d56a7c743e20106865f9cdd7a754a6b
Dipak <dipak@Ashas-MacBook-Pro.local>
2017-03-04 11:33:16 -0800

    Add GifList and GifItems

--- src/index.js
+++ src/index.js
@@ -2,9 +2,31 @@
 import React from 'react'; // base React library includes create/manage components
 import ReactDOM from 'react-dom'; // React DOM to manipulate browser elements
 import SearchBar from './components/SearchBar';
+import GifList from './components/GifList';
 
 // class is new in ES6: below is same as var App = React.createClass({}); in ES5.
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs : [
+        {
+          id: 1,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 2,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 3,
+          url: 'http://fakeimg.pl/300/'
+        }
+      ]
+    }
+  }
 
   handleTermChange(term) {
     console.log(term);
@@ -31,6 +53,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar onTermChange={this.handleTermChange}/>
+        <GifList gifs={this.state.gifs}/>
       </div>
     );
   }
