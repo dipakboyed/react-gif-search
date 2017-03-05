d29bc44820803f719b77b6fede8676925fe1b17e
dipakboyed <dipak@Ashas-MacBook-Pro.local>
2017-03-05 11:00:48 -0800

    add super-agent giphy api

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import GifItem from './GifItem';
 
+/*
+Instead of class GifList extends React.Component, we use  const.
+GifList is a stateless, functional component
+*/
 const GifList = (props) => {
     const gifItems = props.gifs.map((image) => {
         return <GifItem key={image.id} gif={image} />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react'; // base React library includes create/manage componen
 import ReactDOM from 'react-dom'; // React DOM to manipulate browser elements
 import SearchBar from './components/SearchBar';
 import GifList from './components/GifList';
+import request from 'superagent';
 
 // class is new in ES6: below is same as var App = React.createClass({}); in ES5.
 class App extends React.Component {
@@ -11,25 +12,17 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      gifs : [
-        {
-          id: 1,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 2,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 3,
-          url: 'http://fakeimg.pl/300/'
-        }
-      ]
+      gifs : []
     }
   }
 
   handleTermChange(term) {
     console.log(term);
+
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC`;
+    request.get(url, function(err, res) {
+      console.log(res.body.data[0]);
+    });
   }
 
   render() { // render function outputs JSX.
